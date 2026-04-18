83f5c17102db7ea77c88be6d1741d50c4137f6fe
karina1228 <karina_khakimova_2005@mail.ru>
2026-04-18 10:07:53 +0500

    Add files via upload

---
+++ azian.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/karina1228/rikwunn/refs/heads/main/куб%20китай.png",
+
+  "https://raw.githubusercontent.com/karina1228/rikwunn/refs/heads/main/куб%20корея.png",
+
+  "https://raw.githubusercontent.com/karina1228/rikwunn/refs/heads/main/куб%20япония.png",
+
+  "https://raw.githubusercontent.com/karina1228/rikwunn/refs/heads/main/куб%20таиланд.png",
+
+  "https://raw.githubusercontent.com/karina1228/rikwunn/refs/heads/main/куб%20вьетнам.png",
+
+  "https://raw.githubusercontent.com/karina1228/rikwunn/refs/heads/main/куб%20сингапур.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "КИТАЙ",
+
+  "ЮЖНАЯ КОРЕЯ",
+
+  "ЯПОНИЯ",
+
+  "ТАИЛАНД",
+
+  "ВЬЕТНАМ",
+
+  "СИНГАПУР"
+
+];
